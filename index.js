f5a532d0b126d327588f5ff3371135ffca186906
balinux <ryojuniyantara@gmail.com>
2019-05-10 09:46:31 +0800

    initial upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use('/style', express.static(__dirname + '/style'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket) => {
+    socket.username = `Anonymous-${Date.now()}`;
+
+    socket.on('message', (msg) => {
+        io.emit('message', { 'user': socket.username, 'message': msg })
+    })
+
+    socket.on('join', (username) => {
+
+        if (username != null) {
+            socket.username = username
+        }
+        socket.broadcast.emit('message', { 'user': 'Server', 'message': socket.username + ' has joined!' })
+    })
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', { 'user': 'Server', 'message': socket.username + ' leave left chat!' })
+    });
+
+    // socket.on('user left', () =>  {
+    //     socket.broadcast.emit('message', `${socket.username} kabur dari battle` )
+    // })
+})
+http.listen(4000, () => console.log('port on 4000'));
